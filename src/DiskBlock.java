15734b43c053f4d99ba9bb6a84a4f83cce23a552
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2025-11-25 18:59:48 -0400

    danielao

--- src/DiskBlock.java
+++
@@ -1,7 +0,0 @@
-public class DiskBlock {
-    public int index;
-    public boolean free=true;
-    public int next=-1;
-    public String fileName="";
-    public DiskBlock(int index){this.index=index;}
-}
